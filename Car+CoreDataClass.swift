5d7e385f07b424aaeb36720b5945d25c1ba8d0f2
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-06-22 22:03:52 +0300

    ViewController, CoreData

---
+++ Car+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Car+CoreDataClass.swift
+//  MyCars
+//
+//  Created by Marina on 12.04.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Car)
+public class Car: NSManagedObject {
+
+}
